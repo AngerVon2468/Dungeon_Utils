40db3f34b70c1fb2468692df7393ee05de5a9f64
AngerVon2468 <thomaschad0909@gmail.com>
2024-05-26 11:59:09 +1000

    w

--- src/main/java/org/hyrulecraft/dungeon_utils/DungeonUtilsDataGenerator.java
+++ src/main/java/org/hyrulecraft/dungeon_utils/DungeonUtilsDataGenerator.java
@@ -5,6 +5,6 @@ import net.fabricmc.fabric.api.datagen.v1.*;
 public class DungeonUtilsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-		//throw new RuntimeException("Ew datagen!!"); (for legal reasons this is a joke and do not uncomment this)
+		throw new RuntimeException("Ew datagen!!");
 	}
 }
