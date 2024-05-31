22fd7701a4e2ff731710d7426f64dc7c74d700d3
AngerVon2468 <thomaschad0909@gmail.com>
2024-05-31 14:02:06 +1000

    soon:tm:

--- src/main/kotlin/org/hyrulecraft/dungeon_utils/util/event/LoadWorldScreenEvent.kt
+++ src/main/kotlin/org/hyrulecraft/dungeon_utils/util/event/LoadWorldScreenEvent.kt
@@ -3,14 +3,17 @@ package org.hyrulecraft.dungeon_utils.util.event
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents.AFTER_INIT
 
 import net.minecraft.client.gui.screen.LevelLoadingScreen
-import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.MusicSound
 
 import org.hyrulecraft.dungeon_utils.DungeonUtils
-import org.hyrulecraft.dungeon_utils.sound.SoundInit.Companion.START_GAME
+import org.hyrulecraft.dungeon_utils.sound.SoundInit
 
 class LoadWorldScreenEvent {
 
     companion object {
+
+        /*val MENU: MusicSound = MusicSound(SoundInit.START_GAME, 1, 2, true)*/
+
 
         @JvmStatic
         fun titleScreenLoadedEvent() {
@@ -19,7 +22,7 @@ class LoadWorldScreenEvent {
                 if (screen is LevelLoadingScreen) {
 
                     DungeonUtils.LOGGER.info("Loading screen lol")
-                    /*client.player!!.playSound(START_GAME, SoundCategory.MUSIC, 1f, 1f)*/
+
 
                     // Figure out how to play the sound!!
 
