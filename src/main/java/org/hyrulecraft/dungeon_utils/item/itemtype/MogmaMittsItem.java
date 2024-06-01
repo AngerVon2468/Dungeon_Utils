817963b9f2b388b62025f563e3f795766e591c09
AngerVon2468 <thomaschad0909@gmail.com>
2024-06-01 15:21:22 +1000

    just no

--- src/main/java/org/hyrulecraft/dungeon_utils/item/itemtype/MogmaMittsItem.java
+++ src/main/java/org/hyrulecraft/dungeon_utils/item/itemtype/MogmaMittsItem.java
@@ -1,6 +1,9 @@
 package org.hyrulecraft.dungeon_utils.item.itemtype;
 
-import net.minecraft.entity.Entity;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.*;
+import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.nbt.NbtCompound;
@@ -11,6 +14,7 @@ import org.hyrulecraft.dungeon_utils.item.ModItems;
 
 import org.jetbrains.annotations.NotNull;
 
+// TODO: Rework literally all of this.
 public class MogmaMittsItem extends Item {
 
     public MogmaMittsItem(Settings settings) {
@@ -18,21 +22,56 @@ public class MogmaMittsItem extends Item {
     }
 
     @Override
+    public ActionResult useOnBlock(@NotNull ItemUsageContext context) {
+        PlayerEntity player = context.getPlayer();
+        assert player != null;
+        ItemStack stack = player.getStackInHand(player.getActiveHand());
+
+        if (stack.isOf(ModItems.MOGMA_MITTS)) {
+
+            player.setNoGravity(true);
+            player.noClip = true;
+            player.setOnGround(false);
+            player.groundCollision = false;
+            player.setPose(EntityPose.SWIMMING);
+
+        } else {
+
+            player.setNoGravity(false);
+
+        }
+
+        return ActionResult.CONSUME;
+    }
+
+    @Override
     public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
         if (entity instanceof PlayerEntity player) {
-            //
+            ItemStack stack2 = player.getStackInHand(player.getActiveHand());
+            BlockState blockState = world.getBlockState(player.getBlockPos());
+
+            if (stack2.isOf(ModItems.MOGMA_MITTS)) {
+
+                player.noClip = true;
+                player.setOnGround(false);
+                player.groundCollision = false;
+                player.setPose(EntityPose.SWIMMING);
+                if (blockState.isOf(Blocks.AIR)) {
+                    player.addVelocity(0, -0.1, 0);
+                }
+                isBeingUsed(player);
+
+            } else {
+
+                player.setNoGravity(false);
+
+            }
         }
     }
 
     @Override
     public TypedActionResult<ItemStack> use(@NotNull World world, @NotNull PlayerEntity player, @NotNull Hand hand) {
         ItemStack stack = player.getStackInHand(hand);
-
-        if (stack.isOf(ModItems.MOGMA_MITTS)) {
-
-            //
-
-        }
 
         return TypedActionResult.consume(stack);
     }
