3bc850a32e0b95e9fb8988c05ead5fda7edaaeff
AngerVon2468 <thomaschad0909@gmail.com>
2024-06-01 15:38:14 +1000

    a

--- src/main/java/org/hyrulecraft/dungeon_utils/DungeonUtils.java
+++ src/main/java/org/hyrulecraft/dungeon_utils/DungeonUtils.java
@@ -18,7 +18,6 @@ import org.hyrulecraft.dungeon_utils.util.event.*;
 
 import org.slf4j.*;
 
-// TODO: Use player animator or something similar to make the paraglider look better whilst being used. Also adjust paraglider movement variables.
 // TODO: Add climbing! (use a keybind or something idk)
 public class DungeonUtils implements ModInitializer {
 
