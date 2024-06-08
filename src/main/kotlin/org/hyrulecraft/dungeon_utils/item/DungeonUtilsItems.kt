33a786eafdd3ec2624b2b72aac6ab451561ff53b
AngerVon2468 <thomaschad0909@gmail.com>
2024-06-08 15:27:23 +1000

    Hylian Shield

--- src/main/kotlin/org/hyrulecraft/dungeon_utils/item/DungeonUtilsItems.kt
+++ src/main/kotlin/org/hyrulecraft/dungeon_utils/item/DungeonUtilsItems.kt
@@ -1,5 +1,7 @@
 package org.hyrulecraft.dungeon_utils.item
 
+import com.github.crimsondawn45.fabricshieldlib.lib.`object`.FabricShieldItem
+
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 
 import net.minecraft.item.*
@@ -68,10 +70,13 @@ object DungeonUtilsItems {
     val GILDED_SWORD = registerItem("gilded_sword", SwordItem(ToolMaterials.IRON, 10, -2.0f, FabricItemSettings().maxDamage(700)))
 
     @JvmField
-    val THE_MASTER_SWORD = registerItem("the_master_sword", SwordItem(ToolMaterials.DIAMOND, 12, -2.6f, FabricItemSettings().maxDamage(1024)))
+    val THE_MASTER_SWORD = registerItem("the_master_sword", SwordItem(ToolMaterials.DIAMOND, 12, -2.6f, FabricItemSettings().maxDamage(2048)))
 
     @JvmField
-    val THE_MASTER_SWORD_AWAKENED = registerItem("the_master_sword_awakened", SwordItem(ToolMaterials.DIAMOND, 14, -2.8f, FabricItemSettings().maxDamage(2048)))
+    val THE_MASTER_SWORD_AWAKENED = registerItem("the_master_sword_awakened", SwordItem(ToolMaterials.DIAMOND, 14, -2.8f, FabricItemSettings().maxDamage(4096)))
+
+    @JvmField val THE_HYLIAN_SHIELD = registerItem("the_hylian_shield",
+        FabricShieldItem(FabricItemSettings().maxDamage(8192), 10, 100, Items.NETHERITE_BLOCK))
 
     @JvmStatic
     fun registerModItems() {
