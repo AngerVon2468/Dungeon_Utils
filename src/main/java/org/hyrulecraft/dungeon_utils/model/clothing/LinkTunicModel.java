c72a5a82b2ec815d29a9875a49eea0e788fffad4
AngerVon2468 <thomaschad0909@gmail.com>
2024-06-15 16:27:57 +1000

    I'll fix this later

--- src/main/java/org/hyrulecraft/dungeon_utils/model/clothing/LinkTunicModel.java
+++ src/main/java/org/hyrulecraft/dungeon_utils/model/clothing/LinkTunicModel.java
@@ -17,21 +17,9 @@ public class LinkTunicModel extends BipedEntityModel<LivingEntity> {
 		this.leftArm.visible = true;
 	}
 
-	public static TexturedModelData getTexturedModelData() {
+	public static TexturedModelData getTexturedModelData() { // TODO: fix the arms, use the .bbmodel
 		ModelData modelData = BipedEntityModel.getModelData(Dilation.NONE, 0f);
 		ModelPartData modelPartData = modelData.getRoot();
-		ModelPartData left_arm = modelPartData.addChild("left_arm", ModelPartBuilder.create().uv(24, 44).cuboid(-1.0F, -2.0F, -2.0F, 0.0F, 6.0F, 4.0F, new Dilation(0.0F))
-		.uv(16, 44).cuboid(-5.2F, -2.0F, -2.0F, 0.0F, 6.0F, 4.0F, new Dilation(0.0F))
-		.uv(48, 38).cuboid(-5.1F, -2.0F, -2.1F, 4.0F, 6.0F, 0.0F, new Dilation(0.0F))
-		.uv(48, 32).cuboid(-5.1F, -2.0F, 2.1F, 4.0F, 6.0F, 0.0F, new Dilation(0.0F))
-		.uv(20, 0).cuboid(-5.1F, -2.1F, -2.0F, 4.0F, 0.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(5.1F, 24.0F, 0.0F));
-
-		ModelPartData right_arm = modelPartData.addChild("right_arm", ModelPartBuilder.create().uv(8, 44).cuboid(-1.0F, -2.0F, -2.0F, 0.0F, 6.0F, 4.0F, new Dilation(0.0F))
-		.uv(0, 44).cuboid(-5.2F, -2.0F, -2.0F, 0.0F, 6.0F, 4.0F, new Dilation(0.0F))
-		.uv(48, 6).cuboid(-5.1F, -2.0F, -2.1F, 4.0F, 6.0F, 0.0F, new Dilation(0.0F))
-		.uv(48, 0).cuboid(-5.1F, -2.0F, 2.1F, 4.0F, 6.0F, 0.0F, new Dilation(0.0F))
-		.uv(16, 16).cuboid(-5.1F, -2.1F, -2.0F, 4.0F, 0.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(5.1F, 24.0F, 0.0F));
-
 		ModelPartData body = modelPartData.addChild("body", ModelPartBuilder.create().uv(40, 16).cuboid(-4.0F, 3.0F, -2.1F, 8.0F, 8.0F, 0.0F, new Dilation(0.0F))
 		.uv(32, 32).cuboid(-4.0F, 1.0F, 2.1F, 8.0F, 10.0F, 0.0F, new Dilation(0.0F))
 		.uv(40, 38).cuboid(4.1F, 3.0F, -2.0F, 0.0F, 8.0F, 4.0F, new Dilation(0.0F))
@@ -75,13 +63,10 @@ public class LinkTunicModel extends BipedEntityModel<LivingEntity> {
 		.uv(0, 34).cuboid(0.0F, 7.0F, 2.2F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F))
 		.uv(46, 2).cuboid(-1.0F, 6.0F, 2.2F, 1.0F, 1.0F, 0.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
 
-		body.addChild("cube_r1", ModelPartBuilder.create().uv(12, 32).cuboid(-4.0F, -8.0F, 1.0F, 4.0F, 4.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(-4.2F, 18.3F, 2.0F, 0.0F, -1.5708F, 0.1571F));
-
-		body.addChild("cube_r2", ModelPartBuilder.create().uv(24, 4).cuboid(-4.0F, -8.0F, 1.0F, 4.0F, 4.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(6.2F, 18.0F, 2.0F, 0.0F, -1.5708F, -0.1571F));
-
-		body.addChild("cube_r3", ModelPartBuilder.create().uv(40, 24).cuboid(-4.0F, -4.0F, 1.0F, 8.0F, 4.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 14.35F, 1.6F, 0.1571F, 0.0F, 0.0F));
-
-		body.addChild("cube_r4", ModelPartBuilder.create().uv(40, 28).cuboid(-4.0F, -8.0F, 1.0F, 8.0F, 4.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 18.0F, -4.1F, -0.1571F, 0.0F, 0.0F));
+		ModelPartData cube_r1 = body.addChild("cube_r1", ModelPartBuilder.create().uv(12, 32).cuboid(-4.0F, -8.0F, 1.0F, 4.0F, 4.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(-4.2F, 18.3F, 2.0F, 0.0F, -1.5708F, 0.1571F));
+		ModelPartData cube_r2 = body.addChild("cube_r2", ModelPartBuilder.create().uv(24, 4).cuboid(-4.0F, -8.0F, 1.0F, 4.0F, 4.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(6.2F, 18.0F, 2.0F, 0.0F, -1.5708F, -0.1571F));
+		ModelPartData cube_r3 = body.addChild("cube_r3", ModelPartBuilder.create().uv(40, 24).cuboid(-4.0F, -4.0F, 1.0F, 8.0F, 4.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 14.35F, 1.6F, 0.1571F, 0.0F, 0.0F));
+		ModelPartData cube_r4 = body.addChild("cube_r4", ModelPartBuilder.create().uv(40, 28).cuboid(-4.0F, -8.0F, 1.0F, 8.0F, 4.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 18.0F, -4.1F, -0.1571F, 0.0F, 0.0F));
 		return TexturedModelData.of(modelData, 64, 64);
 	}
 }
