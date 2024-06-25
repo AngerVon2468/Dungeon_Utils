3f169b0f84d99473a9da6e19c49cf1618ba2a0c6
AngerVon2468 <thomaschad0909@gmail.com>
2024-06-25 14:21:04 +1000

    slight issue with my random nyooo

--- src/main/java/org/hyrulecraft/dungeon_utils/environment/client/journeymap/DungeonUtilsJourneymapPlugin.java
+++ src/main/java/org/hyrulecraft/dungeon_utils/environment/client/journeymap/DungeonUtilsJourneymapPlugin.java
@@ -9,7 +9,10 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import org.hyrulecraft.dungeon_utils.environment.common.DungeonUtils;
+
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
 
 public class DungeonUtilsJourneymapPlugin implements IClientPlugin {
 
@@ -41,14 +44,88 @@ public class DungeonUtilsJourneymapPlugin implements IClientPlugin {
 
     }
 
+    public int randomColour() {
+        Random random = new Random();
+
+        if (random.nextDouble(1, 16) == 0) {
+
+            return 0x1000000; // Black
+
+        } else if (random.nextDouble(1, 16) == 1) {
+
+            return 0x0000ff; // Blue
+
+        } else if (random.nextDouble(1, 16) == 2) {
+
+            return 0x800000; // Brown
+
+        } else if (random.nextDouble(1, 16) == 3) {
+
+            return 0x00ffff; // Cyan
+
+        } else if (random.nextDouble(1, 16) == 4) {
+
+            return 0x808080; // Grey
+
+        } else if (random.nextDouble(1, 16) == 5) {
+
+            return 0x008000; // Green
+
+        } else if (random.nextDouble(1, 16) == 6) {
+
+            return 0x00ff00; // Lime
+
+        } else if (random.nextDouble(1, 16) == 7) {
+
+            return 0xff00ff; // Magenta
+
+        } else if (random.nextDouble(1, 16) == 8) {
+
+            return 0x000080; // Navy
+
+        } else if (random.nextDouble(1, 16) == 9) {
+
+            return 0xff6600; // Orange
+
+        } else if (random.nextDouble(1, 16) == 10) {
+
+            return 0xff00ff; // Pink
+
+        } else if (random.nextDouble(1, 16) == 11) {
+
+            return 0x800080; // Purple
+
+        } else if (random.nextDouble(1, 16) == 12) {
+
+            return 0xff0000; // Red
+
+        } else if (random.nextDouble(1, 16) == 13) {
+
+            return 0xc0c0c0; // Silver
+
+        } else if (random.nextDouble(1, 16) == 14) {
+
+            return 0xffffff; // White
+
+        } else if (random.nextDouble(1, 16) == 15) {
+
+            return 	0xffff00; // Yellow
+
+        } else {
+
+            return 0;
+
+        }
+    }
+
     public Waypoint createWaypoint(BlockPos blockPos, RegistryKey<World> dimension) {
         Waypoint waypoint = null;
         try {
 
-            // Waypoint itself
-            waypoint = new Waypoint(DungeonUtils.MOD_ID, "dg_test", "DG_Test", dimension, blockPos)
-                    .setColor(0x00ffff)
-                    .setPersistent(true);
+            waypoint = new Waypoint(DungeonUtils.MOD_ID, blockPos.getX() + "_" + blockPos.getZ(), blockPos.getX() + ", " + blockPos.getZ(), dimension, blockPos)
+                    .setColor(randomColour())
+                    .setPersistent(true)
+                    .setEditable(false);
 
             // Add or update
             this.jmAPI.show(waypoint);
