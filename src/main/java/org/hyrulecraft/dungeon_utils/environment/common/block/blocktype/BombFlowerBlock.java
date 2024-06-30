4c601da08ce3ef91aa2b71e3b7e22093f3709f8a
AngerVon2468 <thomaschad0909@gmail.com>
2024-06-30 11:57:29 +1000

    more edits to bomb

--- src/main/java/org/hyrulecraft/dungeon_utils/environment/common/block/blocktype/BombFlowerBlock.java
+++ src/main/java/org/hyrulecraft/dungeon_utils/environment/common/block/blocktype/BombFlowerBlock.java
@@ -3,22 +3,14 @@ package org.hyrulecraft.dungeon_utils.environment.common.block.blocktype;
 import net.minecraft.block.*;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.mob.RavagerEntity;
-import net.minecraft.item.ItemConvertible;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.item.*;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
-import net.minecraft.state.property.IntProperty;
-import net.minecraft.state.property.Properties;
-import net.minecraft.state.property.Property;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.state.property.*;
+import net.minecraft.util.math.*;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.world.BlockView;
-import net.minecraft.world.GameRules;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldView;
+import net.minecraft.world.*;
 
 import org.hyrulecraft.dungeon_utils.environment.common.item.DungeonUtilsItems;
 
@@ -32,6 +24,7 @@ public class BombFlowerBlock extends PlantBlock implements Fertilizable {
         this.setDefaultState((BlockState)((BlockState)this.stateManager.getDefaultState()).with(AGE, 0));
     }
 
+    @SuppressWarnings("deprecation")
     public VoxelShape getOutlineShape(BlockState blockState, BlockView blockView, BlockPos blockPos, ShapeContext shapeContext) {
         return AGE_TO_SHAPE[this.getAge(blockState)];
     }
@@ -60,12 +53,12 @@ public class BombFlowerBlock extends PlantBlock implements Fertilizable {
         return !this.isMature(blockState);
     }
 
+    @SuppressWarnings("deprecation")
     public void randomTick(BlockState blockState, ServerWorld serverWorld, BlockPos blockPos, Random random) {
         if (serverWorld.getBaseLightLevel(blockPos, 0) >= 9) {
             int i = this.getAge(blockState);
             if (i < this.getMaxAge()) {
-                float f = getAvailableMoisture(this, serverWorld, blockPos);
-                if (random.nextInt((int)(25.0F / f) + 1) == 0) {
+                if (random.nextInt(6 / 2) == 0) {
                     serverWorld.setBlockState(blockPos, this.withAge(i + 1), 2);
                 }
             }
@@ -87,51 +80,11 @@ public class BombFlowerBlock extends PlantBlock implements Fertilizable {
         return MathHelper.nextInt(world.random, 2, 5);
     }
 
-    protected static float getAvailableMoisture(Block block, BlockView blockView, BlockPos blockPos) {
-        float f = 1.0F;
-        BlockPos blockPos2 = blockPos.down();
-
-        for(int i = -1; i <= 1; ++i) {
-            for(int j = -1; j <= 1; ++j) {
-                float g = 0.0F;
-                BlockState blockState = blockView.getBlockState(blockPos2.add(i, 0, j));
-                if (blockState.isOf(Blocks.FARMLAND)) {
-                    g = 1.0F;
-                    if ((Integer)blockState.get(FarmlandBlock.MOISTURE) > 0) {
-                        g = 3.0F;
-                    }
-                }
-
-                if (i != 0 || j != 0) {
-                    g /= 4.0F;
-                }
-
-                f += g;
-            }
-        }
-
-        BlockPos blockPos3 = blockPos.north();
-        BlockPos blockPos4 = blockPos.south();
-        BlockPos blockPos5 = blockPos.west();
-        BlockPos blockPos6 = blockPos.east();
-        boolean bl = blockView.getBlockState(blockPos5).isOf(block) || blockView.getBlockState(blockPos6).isOf(block);
-        boolean bl2 = blockView.getBlockState(blockPos3).isOf(block) || blockView.getBlockState(blockPos4).isOf(block);
-        if (bl && bl2) {
-            f /= 2.0F;
-        } else {
-            boolean bl3 = blockView.getBlockState(blockPos5.north()).isOf(block) || blockView.getBlockState(blockPos6.north()).isOf(block) || blockView.getBlockState(blockPos6.south()).isOf(block) || blockView.getBlockState(blockPos5.south()).isOf(block);
-            if (bl3) {
-                f /= 2.0F;
-            }
-        }
-
-        return f;
-    }
-
     public boolean canPlaceAt(BlockState blockState, WorldView worldView, BlockPos blockPos) {
         return (worldView.getBaseLightLevel(blockPos, 0) >= 8 || worldView.isSkyVisible(blockPos)) && super.canPlaceAt(blockState, worldView, blockPos);
     }
 
+    @SuppressWarnings("deprecation")
     public void onEntityCollision(BlockState blockState, World world, BlockPos blockPos, Entity entity) {
         if (entity instanceof RavagerEntity && world.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING)) {
             world.breakBlock(blockPos, true, entity);
