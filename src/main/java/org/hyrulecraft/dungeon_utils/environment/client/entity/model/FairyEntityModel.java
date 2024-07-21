cbb8505c552cbceabba52bdf36663b97c4b3c5fe
AngerVon2468 <thomaschad0909@gmail.com>
2024-07-21 20:13:02 +1000

    testing with ghast movement control

--- src/main/java/org/hyrulecraft/dungeon_utils/environment/client/entity/model/FairyEntityModel.java
+++ src/main/java/org/hyrulecraft/dungeon_utils/environment/client/entity/model/FairyEntityModel.java
@@ -31,12 +31,12 @@ public class FairyEntityModel extends EntityModel<FairyEntity> {
 		.uv(12, 9).cuboid(-3.0F, -5.0F, -2.0F, 1.0F, 4.0F, 4.0F, new Dilation(0.0F))
 		.uv(12, 4).cuboid(-2.0F, -6.0F, -2.0F, 4.0F, 1.0F, 4.0F, new Dilation(0.0F))
 		.uv(0, 8).cuboid(-2.0F, -1.0F, -2.0F, 4.0F, 1.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
-		main.addChild("cube_r1", ModelPartBuilder.create().uv(7, 21).cuboid(0.3725F, -3.5F, 2.163F, 0.0F, 1.0F, 1.2F, new Dilation(0.0F))
+		main.addChild("cube_r1", ModelPartBuilder.create().uv(8, 22).cuboid(0.3725F, -3.5F, 2.163F, 0.0F, 1.0F, 1.0F, new Dilation(0.0F))
 		.uv(23, 9).cuboid(0.3725F, -9.5F, 4.153F, 0.0F, 5.0F, 1.0F, new Dilation(0.0F))
 		.uv(7, 21).cuboid(0.3725F, -2.5F, 2.163F, 0.0F, 2.0F, 2.0F, new Dilation(0.0F))
 		.uv(1, 2).cuboid(0.3725F, -8.5F, 3.163F, 0.0F, 1.0F, 1.0F, new Dilation(0.0F))
 		.uv(7, 21).cuboid(0.3725F, -7.5F, 2.163F, 0.0F, 4.0F, 2.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.5F, 0.5268F, 0.0F, 0.7854F, 0.0F));
-		main.addChild("cube_r2", ModelPartBuilder.create().uv(7, 21).cuboid(0.0F, -1.5F, -1.495F, 0.0F, 1.0F, 1.2F, new Dilation(0.0F))
+		main.addChild("cube_r2", ModelPartBuilder.create().uv(8, 21).cuboid(0.0F, -1.5F, -1.495F, 0.0F, 1.0F, 1.0F, new Dilation(0.0F))
 		.uv(8, 22).cuboid(0.0F, 1.5F, -0.505F, 0.0F, 1.0F, 1.0F, new Dilation(0.0F))
 		.uv(7, 21).cuboid(0.0F, -0.5F, -1.495F, 0.0F, 2.0F, 2.0F, new Dilation(0.0F))
 		.uv(8, 22).cuboid(0.0F, 0.5F, 0.495F, 0.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(-2.85F, -1.5F, 2.85F, 0.0F, -0.7854F, 0.0F));
--- src/main/java/org/hyrulecraft/dungeon_utils/environment/common/entity/entity_type/FairyEntity.java
+++ src/main/java/org/hyrulecraft/dungeon_utils/environment/common/entity/entity_type/FairyEntity.java
@@ -1,9 +1,13 @@
 package org.hyrulecraft.dungeon_utils.environment.common.entity.entity_type;
 
 import net.minecraft.entity.*;
+import net.minecraft.entity.ai.control.MoveControl;
 import net.minecraft.entity.attribute.*;
 import net.minecraft.entity.mob.*;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.*;
 
 import org.hyrulecraft.dungeon_utils.environment.common.entity.goal.FlyRandomlyGoal;
@@ -19,6 +23,7 @@ public class FairyEntity extends FlyingEntity implements Tameable {
     public FairyEntity(EntityType<? extends FlyingEntity> type, World world) {
         super(type, world);
         this.owner = null;
+        this.moveControl = new FairyMoveControl(this);
     }
 
     public static DefaultAttributeContainer.Builder createFairyAttributes() {
@@ -43,13 +48,13 @@ public class FairyEntity extends FlyingEntity implements Tameable {
     @Override
     protected void initGoals() {
         super.initGoals();
-        this.goalSelector.add(1, new FlyRandomlyGoal(this));
+        this.goalSelector.add(4, new FlyRandomlyGoal(this));
     }
 
     @Nullable
     @Override
     public UUID getOwnerUuid() {
-        return owner.getUuid();
+        return owner != null ? owner.getUuid() : null;
     }
 
     @Override
@@ -60,5 +65,45 @@ public class FairyEntity extends FlyingEntity implements Tameable {
     @Override
     public @Nullable LivingEntity getOwner() {
         return this.owner;
+    }
+
+    static class FairyMoveControl extends MoveControl {
+        private final FairyEntity fairy;
+        private int collisionCheckCooldown;
+
+        public FairyMoveControl(FairyEntity fairy) {
+            super(fairy);
+            this.fairy = fairy;
+        }
+
+        public void tick() {
+            if (this.state == State.MOVE_TO) {
+                if (this.collisionCheckCooldown-- <= 0) {
+                    this.collisionCheckCooldown += this.fairy.getRandom().nextInt(5) + 2;
+                    Vec3d vec3d = new Vec3d(this.targetX - this.fairy.getX(), this.targetY - this.fairy.getY(), this.targetZ - this.fairy.getZ());
+                    double d = vec3d.length();
+                    vec3d = vec3d.normalize();
+                    if (this.willCollide(vec3d, MathHelper.ceil(d))) {
+                        this.fairy.setVelocity(this.fairy.getVelocity().add(vec3d.multiply(0.1)));
+                    } else {
+                        this.state = State.WAIT;
+                    }
+                }
+
+            }
+        }
+
+        private boolean willCollide(Vec3d vec3d, int i) {
+            Box box = this.fairy.getBoundingBox();
+
+            for(int j = 1; j < i; ++j) {
+                box = box.offset(vec3d);
+                if (!this.fairy.getWorld().isSpaceEmpty(this.fairy, box)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
